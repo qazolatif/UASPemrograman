f7f36a99f3d79eb8534d3214cd23ea737c35d828
Khias Nurlatif Ari Subekti <57320303+qazolatif@users.noreply.github.com>
2020-07-20 14:42:40 +0700

    Update Mahasiswa.cs

--- ClassInduk/Mahasiswa.cs
+++ ClassInduk/Mahasiswa.cs
@@ -6,58 +6,10 @@ namespace UASPemrograman.Mahasiswa1
 {
     public abstract class Mahasiswa
     {
-        private string nim;
-        private string nama;
-        private string kelas;
-        private string prodi;
-
-        public string NIM
-        {
-            get
-            {
-                return nim;
-            }
-            set
-            {
-                nim = value;
-            }
-        }
-
-        public string Nama
-        {
-            get
-            {
-                return nama;
-            }
-            set
-            {
-                nama = value;
-            }
-        }
-
-        public string Kelas
-        {
-            get
-            {
-                return kelas;
-            }
-            set
-            {
-                kelas = value;
-            }
-        }
-
-        public string Prodi
-        {
-            get
-            {
-                return prodi;
-            }
-            set
-            {
-                prodi = value;
-            }
-        }
+        public string NIM { get; set; }
+        public string Nama { get; set; }
+        public string Kelas { get; set; }
+        public string Prodi { get; set; }
 
         public virtual string Status()
         {
